947a1f1b99616c4b1f1c575d9eaeacdd1f9f61cf
Stefan Quest <stefan.quest@gmail.com>
2018-07-20 22:55:57 +0200

    added first components added compnents folder and reducers folder

---
+++ hangman/src/components/hangman.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+
+class Hangman extends Component {
+    componentDidMount() {
+        console.log(123);
+    }
+  render() {
+    return (
+      <div>
+        <h1> This is  my hangman game</h1>
+      </div>
+    )
+  }
+}
+
+export default Hangman;
